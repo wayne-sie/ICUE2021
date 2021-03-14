dbd23720254f0d2b0704fcf5eecb0011fdbf89c4
Sanjeev Sritharan <35245440+sanjeev2001@users.noreply.github.com>
2021-03-14 11:07:25 -0400

    implemented text messaging

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,10 @@ from flask import render_template, redirect, request
 from pymongo import MongoClient
 from flask import request
 import datetime
+import os
+from twilio.rest import Client
 
-
+restaurantName = "Starbucks"
 
 @app.route("/")
 @app.route("/index")
@@ -37,22 +39,56 @@ def index():
 @app.route("/messaged", methods=['GET', 'POST'])
 def messaged():
     if request.method == 'POST':
-        # account_sid = os.environ['ACc40d883d6878ff32d457ab4c0d38216c']
-        # auth_token = os.environ['b3130d8ddd38c115d9cf4990b5468e38']
-        # client = Client(account_sid, auth_token)
 
-        rfidOfInfected = str(request.form.getlist('checked'))
+        rfidOfInfected = request.form.getlist('checked')
 
         cluster = MongoClient('mongodb+srv://sanjeev2001:mEm39dShwBgbf2@cluster0.w43vk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
         db = cluster["customer"]
         history = db["history"]
-        query = {'rfid': rfidOfInfected}
+        query = {'rfid': rfidOfInfected[0]}
         infectedPerson = history.find_one(query)
-        for i in range(len(history['rfid'])):
-            historyTime = datetime.datetime.striptime(history['enter_time'], '%d-%m-%y %H:%M:%S')
-            infectedTime = datetime.datetime.striptime(infected['enter_time'], '%d-%m-%y %H:%M:%S')
-            if historyTime > infectedTime:
-                return "True"
+
+        # return query['rfid']
+
+        historyArr = {
+            "rfid": [],
+            'phone_num' : [],
+            'enter_time' : [],
+            'exit_time' : [],
+            'is_infected' : []
+        }
+
+        for document in history.find():
+            historyArr['rfid'].append(document['rfid'])
+            historyArr['phone_num'].append(document['phone_num'])
+            historyArr['enter_time'].append(document['enter_time'])
+            historyArr['exit_time'].append(document['exit_time'])
+            historyArr['is_infected'].append(document['is_infected'])
+
+        messageList = []
+        enterList = []
+        exitList = []
+        for i in range(len(historyArr['rfid'])):
+            # historyTime = datetime.datetime.strptime(historyArr['enter_time'][i], '%d-%m-%y %H:%M:%S')
+            # infectedTime = datetime.datetime.strptime(infectedPerson['enter_time'], '%d-%m-%y %H:%M:%S')
+
+            
+            if historyArr['enter_time'][i] > infectedPerson['enter_time']:
+
+                messageList.append(historyArr['phone_num'][i])
+                enterList.append(historyArr['enter_time'][i])
+                exitList.append(historyArr['exit_time'][i])
+
+        for i in range(len(messageList)):
+            # message = client.messages \
+            #     .create(
+            #          body=f"You have been in contact with someone that has tested positive for COVID-19. Please make sure to get tested and self quarantine. During your stay at {restaurantName} from {enterList[i]} to {exitList[i]}.",
+            #          from_='+14139923391',
+            #          to=f'+1{messageList[i]}'
+            #      )
+
+        # return str(messageList)
+        return render_template()
 
     else: 
         return "Error 404 not found"
